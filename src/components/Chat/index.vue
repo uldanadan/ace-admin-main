d89a8fbbaf0b1b8891c27cbdae298a8a4c74a9fd
Akhmed <id.shakirov.akhmed@gmail.com>
2024-06-03 18:16:38 +0600

    preparing to connect a web socket

--- src/components/Chat/index.vue
+++ src/components/Chat/index.vue
@@ -7,17 +7,20 @@
 
         <Box
             v-model="isShow"
-            v-model:idSocket="idSocket"
+            v-model:idPlayer="idPlayer"
         >
             <div class="chat__transition">
                 <Players
                     class="chat__transition-left"
-                    :class="{ 'active': !idSocket }"
-                    @setIdSocket="(id: number) => idSocket = id"
+                    :class="{ 'active': !idPlayer }"
+                    :players="players"
+                    @setIdPlayer="(id: number) => idPlayer = id"
                 />
                 <Chat
                     class="chat__transition-right"
-                    :class="{ 'active': idSocket }"
+                    :class="{ 'active': idPlayer }"
+                    :messages="messages[idPlayer]"
+                    @setMessage="setMessage"
                 />
             </div>
         </Box>
@@ -26,10 +29,70 @@
 
 <script setup lang="ts">
 import { Players, Chat, Box } from './components'
-import { ref } from 'vue'
+import { ref, onMounted, onBeforeUnmount } from 'vue'
 
 const isShow = ref<boolean>(false)
-const idSocket = ref<number>(0)
+
+const idAdmin = ref<number | string>('admin')
+const idPlayer = ref<number>(0)
+
+const players = ref<any[]>([
+    { id: 1, text: 'Полный' },
+    { id: 2, text: '' },
+    { id: 3, text: '' },
+    { id: 4, text: '' },
+    { id: 5, text: '' },
+    { id: 6, text: '' },
+    { id: 7, text: '' },
+    { id: 8, text: '' },
+    { id: 9, text: '' },
+    { id: 10, text: '' },
+    { id: 11, text: '' },
+    { id: 12, text: '' },
+    { id: 13, text: '' },
+    { id: 14, text: '' },
+])
+
+const sockets = ref<{ [key: string]: WebSocket }>({})
+const messages = ref<{ [key: string]: any[] }>({
+    1: [
+        { isAdmin: true, id: 4, text: 'Here is the estimated. Please check', date: '16:03' },
+        { isAdmin: false, id: 3, text: 'Please send me a estimated cost', date: '16:02' },
+        { isAdmin: true, id: 2, text: 'Hello, Yes I can make this', date: '16:01' },
+        { isAdmin: false, id: 1, text: 'Hey Zakir, I want to make this type of workspace. Can you help me?', date: '16:00' },
+    ]
+})
+
+onMounted(() => {
+    players.value.forEach((player: any) => {
+        const ws = new WebSocket(`ws://localhost:8080?id=${idAdmin.value}&user=${player?.id}`)
+        sockets.value[player?.id] = ws
+        messages.value[player?.id] = []
+
+        ws.onmessage = (event) => {
+            const data = JSON.parse(event.data)
+            messages.value[player?.id].unshift(data.message)
+        }
+    })
+})
+
+onBeforeUnmount(() => {
+    Object.values(sockets.value).forEach((ws) => ws.close())
+})
+
+const setMessage = (message: any) => {
+    const ws = sockets.value[idPlayer.value]
+
+    if (ws && Object.keys(message)?.length) {
+
+        ws.send(JSON.stringify({ 
+            targetId: idPlayer.value,
+            message: message 
+        }))
+
+        messages.value[idPlayer.value].unshift(message)
+    }
+}
 </script>
 
 <style scoped lang="scss">
@@ -104,15 +167,15 @@ const idSocket = ref<number>(0)
 }
 
 @keyframes pulse {
-  0% {
-    transform: scale(1, 1);
-  }
-  50% {
-    opacity: 0.3;
-  }
-  100% {
-    transform: scale(1.5);
-    opacity: 0;
-  }
+    0% {
+        transform: scale(1, 1);
+    }
+    50% {
+        opacity: 0.3;
+    }
+    100% {
+        transform: scale(1.5);
+        opacity: 0;
+    }
 }
 </style>
