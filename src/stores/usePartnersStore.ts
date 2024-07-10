1e66b42a40d346d698fc037c9ae281429c1b111a
uldanadan <uabdiganiyevaa@gmail.com>
2024-07-11 02:17:03 +0500

    update computers

--- src/stores/usePartnersStore.ts
+++ src/stores/usePartnersStore.ts
@@ -1,42 +1,18 @@
 import { defineStore } from "pinia";
 import { ref, computed } from "vue";
 import PartnersDataService from "@/services/PartnersDataService.ts";
-import { GetParams, Computer } from "@/types/types"
+import { Computer } from "@/types/types"
 
 export const usePartnersStore = defineStore("partners", () => {
-	const computers = ref([]);
 	const gameCenters = ref([]);
 
 	const loadGameCenters = async () => {
 		const response = await PartnersDataService.getGameCenters();
 		gameCenters.value = response.data.results;
 	}
-	const loadComputers = async () => {
-		try {
-			const queries = [];
-			if (Array.isArray(gameCenters.value)) {
-				for (const gameCenter of gameCenters.value) {
-					for (const zone of gameCenter.zones) {
-						queries.push(PartnersDataService.getComputersFromZone(gameCenter.uuid, zone.uuid));
-					}
-				}
-				const responses = await Promise.all(queries);
-				const allComputers = responses.flatMap(response => response.data);
-				computers.value = allComputers;
-			} else {
-				console.error('gameCenters is not an array:');
-			}
-		} catch (error) {
-			console.error('Failed to load computers:', error);
-		}
-	}
-
 	const postComputer = async (data: Computer) => {
 		await PartnersDataService.postComputer(data);
 	}
-	const getComputers = computed(() => {
-		return computers.value;
-	})
 	const getGameCenters = computed(() => {
 		return gameCenters.value;
 	})
@@ -44,5 +20,5 @@ export const usePartnersStore = defineStore("partners", () => {
 		const storage = localStorage.getItem('selectedGameCenter');
 		return storage ? JSON.parse(storage) : undefined;
 	})
-	return { computers, getComputers, getSelectedGameCenter, loadComputers, postComputer, loadGameCenters, gameCenters, getGameCenters }
+	return { getSelectedGameCenter, postComputer, loadGameCenters, gameCenters, getGameCenters }
 })
